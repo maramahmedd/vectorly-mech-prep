303e41de92774af7ef51b985fd592cee2bf51fa4
maram-ahmed1 <maram1@berkeley.edu>
2025-09-20 10:52:33 -0700

    swtiched to custom domain name

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/vectorly-mech-prep/",
+  base: "/",
   server: {
     host: "::",
     port: 8080,
