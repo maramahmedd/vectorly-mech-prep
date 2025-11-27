af2b337aaa34881c67639147465e5bf246abecfa
maram-ahmed1 <maram1@berkeley.edu>
2025-11-26 16:33:36 -0800

    Updates

---
+++ assets/skeleton-BLvE16zE.js
@@ -0,0 +1,6 @@
+import{c as s,j as o,g as t}from"./index-CCO05Osv.js";/**
+ * @license lucide-react v0.462.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const n=s("Clock",[["circle",{cx:"12",cy:"12",r:"10",key:"1mglay"}],["polyline",{points:"12 6 12 12 16 14",key:"68esgv"}]]);function l({className:e,...c}){return o.jsx("div",{"data-slot":"skeleton",className:t("bg-accent animate-pulse rounded-md",e),...c})}export{n as C,l as S};
